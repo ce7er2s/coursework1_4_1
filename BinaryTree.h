e5f69e40a5fcbb5793ea7338bda410cf4417ad38
Foko <spike.calhun@yandex.ru>
2022-03-30 17:23:37 +0300

    -

--- BinaryTree.h
+++ BinaryTree.h
@@ -14,26 +14,75 @@ private:
         Node* right = nullptr;
         KEY key;
         ITEM item;
+        uint32_t count = 0;
 
-    private:
-        bool addPair(const std::string key, const std::string item) {
+    public:
+        Node () = default;
+        Node (KEY _key, ITEM _item): key(_key), item(_item), count(1) {
+            ;
+        }
 
+        void insert(const std::string new_key, const std::string new_item) {
+            char pos = key.compare(new_key);
+            if (pos == -1) { //  если ключ меньше текущего значения, сливаем его влево
+                if (left != nullptr) { // выделяем новый узел, если его нет, либо рекурсивно отдаем его
+                    left->insert(new_key, new_item);
+                } else {
+                    left = new Node(new_key, new_item);
+                }
+            } else if (pos == 1) { // аналогично вправо
+                if (right != nullptr) {
+                    right->insert(new_key, new_item);
+                } else {
+                    right = new Node(new_key, new_item);
+                }
+            } else { // если текущий ключ узла равен новому ключу, прибавляем 1
+                ++count;
+            }
+        }
+
+        ITEM search(const std::string& search_key) {
+            char pos = key.compare(search_key);
+            if (pos == 0) {
+                return key;
+            } else if (pos == -1) {
+                if (left != nullptr) {
+                    return left->search(search_key);
+                } else {
+                    return "";
+                }
+            } else {
+                if (right != nullptr) {
+                    return right->search(search_key);
+                } else {
+                    return "";
+                }
+            }
         }
     };
 
     Node* head;
     size_t size;
 
-    BinaryTree(); // стандартная инициализация
+public:
+    BinaryTree () = default;// стандартная инициализация
 
-    BinaryTree(KEY key, ITEM item); // конструктор создания словаря из одной пары значений
+    BinaryTree (KEY key, ITEM item) {
+        head = new BinaryTree::Node(key, item);
+        size = 1;
+    }; // конструктор создания словаря из одной пары значений
 
-    BinaryTree(const BinaryTree& tree) = default;
+    BinaryTree (const BinaryTree& tree) = default;
 
-    BinaryTree(BinaryTree&& tree) noexcept = default;
+    BinaryTree (BinaryTree&& tree) noexcept = default;
 
-    bool addPair (const std::string key, const std::string item) {
-        return head->addPair(key, item)
+    inline void insert (const std::string key, const std::string item) {
+        head->insert(key, item);
+        ++size;
+    }
+
+    inline ITEM search (const std::string& search_key) {
+        return head->search(search_key);
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include "BinaryTree.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    BinaryTree<std::string, std::string> tree("item", "key");
+    std::string command;
+    while (command != "exit") {
+        std::vector<std::string> parsed_command = {};
+        std::getline(std::cin, command);
+        size_t pos;
+        std::string token;
+        while ((pos = command.find(' ')) != std::string::npos) {
+            token = command.substr(0, pos);
+            if (!token.empty())
+                parsed_command.push_back(token);
+            command.erase(0, pos+1);
+        }
+        token = command.substr(0, pos);
+        if (!token.empty())
+            parsed_command.push_back(token);
+
+        if (parsed_command[0] == "insert") {
+            for (size_t i = 1; i < parsed_command.size(); i+=2) {
+                tree.insert(parsed_command[i], parsed_command[i+1]);
+            }
+        } else if (parsed_command[1] == "search") {
+            for (size_t i = 1; i < parsed_command.size(); ++i) {
+                std::string item = tree.search(parsed_command[i]);
+                if (!item.empty()) {
+                    std::cout << "FOUND key " << parsed_command[i] << " with item " << item << "\n";
+                } else {
+                    std::cout << "NO item with key " << parsed_command[i] << " was found\n";
+                }
+            }
+        }
+    }
     return 0;
 }
