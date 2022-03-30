45c7746f924cde9019b7e45b9b9ebc5d85473168
Foko <spike.calhun@yandex.ru>
2022-03-30 16:04:17 +0300

    -

--- BinaryTree.h
+++ BinaryTree.h
@@ -5,4 +5,36 @@
 #ifndef COURSEWORK1_4_1_BINARYTREE_H
 #define COURSEWORK1_4_1_BINARYTREE_H
 
+#include <string>
+
+template <typename KEY, typename ITEM> class BinaryTree {
+private:
+    struct Node {
+        Node* left = nullptr;
+        Node* right = nullptr;
+        KEY key;
+        ITEM item;
+
+    private:
+        bool addPair(const std::string key, const std::string item) {
+
+        }
+    };
+
+    Node* head;
+    size_t size;
+
+    BinaryTree(); // стандартная инициализация
+
+    BinaryTree(KEY key, ITEM item); // конструктор создания словаря из одной пары значений
+
+    BinaryTree(const BinaryTree& tree) = default;
+
+    BinaryTree(BinaryTree&& tree) noexcept = default;
+
+    bool addPair (const std::string key, const std::string item) {
+        return head->addPair(key, item)
+    }
+};
+
 #endif //COURSEWORK1_4_1_BINARYTREE_H
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "BinaryTree.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
